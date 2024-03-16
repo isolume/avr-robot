e59dcd0d6ecbafbaa07c6d7b253bf68439c19f53
nerd <nerd@n3rd3x3.dev>
2024-03-15 21:50:44 -0500

    add reading for pin a0 this will hopefully allow infrared sensor to be tested

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::Adc;
 use arduino_hal::hal::wdt;
 use panic_halt as _;
 use motor_shield::{init_ams, MotorCommands};
@@ -12,6 +13,11 @@ use motor_shield::MotorPort;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    let mut adc = Adc::new(dp.ADC, Default::default());
+    let a0 = pins.a0.into_analog_input(&mut adc);
+    let a0read = a0.analog_read(&mut adc);
 
     let mut motor_shield = init_ams!(
         ShieldLayout {
@@ -30,8 +36,9 @@ fn main() -> ! {
     motor.run(MotorCommands::FORWARD);
 
     loop {
+        ufmt::uwriteln!(&mut serial, "A0 reading: {}", a0read).unwrap();
         motor.speed(255);
-        arduino_hal::delay_ms(1000);
+        arduino_hal::delay_ms(2000);
         motor.speed(0);
         watchdog.feed();
     }
